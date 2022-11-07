e6b3a1fa3c1d1d8bf10c1e1c247085e4a2508c74
Farukov <vladislavfarukov2004@gmail.com>
2022-11-07 18:18:02 +0700

    Commit 1

--- Inp.py
+++
@@ -1,5 +0,0 @@
-from multiprocessing import Process,Pipe
-
-msg = 1
-def f(child_conn):
-    child_conn.send(msg)
--- Out.py
+++
@@ -1,6 +0,0 @@
-from multiprocessing import Process,Queue,Pipe
-from Inp import f
-
-parent_conn, child_conn = Pipe()
-f(child_conn)
-print(parent_conn.recv())
